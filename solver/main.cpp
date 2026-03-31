f0ef996df8e33d4c8d8f89558c53feb94a26aa69
Tomas Turek <tom.turek@atlas.cz>
2026-03-31 14:04:54 +0200

    added comments

--- solver/main.cpp
+++ solver/main.cpp
@@ -13,9 +13,14 @@ void process_input_file(const std::string &file) {
   std::cout << "# File \"" << GREEN << file << RESET << "\" contains " << GREEN
             << input.get_tree_count() << RESET << " trees with " << GREEN
             << input.get_leaf_count() << RESET << " leafs each." << std::endl;
+
+  // Contract cherries to reduce size.
   input.contract_cherries();
+  // Find the solution.
   auto edges_to_erase = ilp(input);
+  // Remove the edges.
   auto output = input.remove_edges(edges_to_erase);
+  // Print the result.
   for (auto &&tree : output) {
     if (!tree->is_empty())
       tree->write(input.get_contractions());
