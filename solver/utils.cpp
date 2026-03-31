1dcc8736d2133eb1576154235dfda9b739243cbe
Tomas Turek <tom.turek@atlas.cz>
2026-03-31 14:03:29 +0200

    changed order of functions

--- solver/utils.cpp
+++ solver/utils.cpp
@@ -1,29 +1,6 @@
 #include "utils.h"
 #include <sstream>
 
-std::vector<std::string> split(const std::string &str) {
-  std::vector<std::string> tokens;
-  std::istringstream iss(str);
-  std::string token;
-  while (iss >> token) {
-    tokens.push_back(token);
-  }
-  return tokens;
-}
-
-std::vector<std::string> split(const std::string &str, char delimiter) {
-  std::vector<std::string> tokens;
-  size_t start = 0;
-  size_t end = str.find(delimiter);
-  while (end != std::string::npos) {
-    tokens.push_back(str.substr(start, end - start));
-    start = end + 1;
-    end = str.find(delimiter, start);
-  }
-  tokens.push_back(str.substr(start));
-  return tokens;
-}
-
 LCA_KEY get_lca_key(int first, int second) {
   std::ostringstream os;
   if (first > second) {
@@ -49,4 +26,27 @@ LCA_KEY get_lca_key(int first, int second, int third) {
 
 bool is_approx_one(float x, float tolerance) {
   return std::abs(x - 1.0f) < tolerance;
+}
+
+std::vector<std::string> split(const std::string &str) {
+  std::vector<std::string> tokens;
+  std::istringstream iss(str);
+  std::string token;
+  while (iss >> token) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+std::vector<std::string> split(const std::string &str, char delimiter) {
+  std::vector<std::string> tokens;
+  size_t start = 0;
+  size_t end = str.find(delimiter);
+  while (end != std::string::npos) {
+    tokens.push_back(str.substr(start, end - start));
+    start = end + 1;
+    end = str.find(delimiter, start);
+  }
+  tokens.push_back(str.substr(start));
+  return tokens;
 }
--- solver/utils.h
+++ solver/utils.h
@@ -18,17 +18,6 @@ const std::string GREEN = "\033[32m";
 const std::string YELLOW = "\033[33m";
 const std::string RESET = "\033[0m";
 
-/// Split strings by space.
-/// @param str Input string.
-/// @return Vector of all substrings.
-std::vector<std::string> split(const std::string &str);
-
-/// Split strings by delimiter.
-/// @param str Input string.
-/// @param delimiter Given delimiter.
-/// @return Vector of all substrings.
-std::vector<std::string> split(const std::string &str, char delimiter);
-
 /// Get the lca key for two leafs sort them and put in string "first"#"second".
 /// @param first First leaf.
 /// @param second Second leaf.
@@ -49,4 +38,14 @@ LCA_KEY get_lca_key(int first, int second, int third);
 /// @return Whether x is at most tolerance far away from 1.
 bool is_approx_one(float x, float tolerance = 1e-6f);
 
+/// Split strings by space.
+/// @param str Input string.
+/// @return Vector of all substrings.
+std::vector<std::string> split(const std::string &str);
+
+/// Split strings by delimiter.
+/// @param str Input string.
+/// @param delimiter Given delimiter.
+/// @return Vector of all substrings.
+std::vector<std::string> split(const std::string &str, char delimiter);
 #endif
