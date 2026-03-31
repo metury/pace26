63955ea2157d7296b1db6957bde512a33f8316e7
Tomas Turek <tom.turek@atlas.cz>
2026-03-31 14:02:18 +0200

    added doxygen comments

--- solver/utils.h
+++ solver/utils.h
@@ -1,11 +1,17 @@
+/// @file utils.h
+/// @brief Common utility functions.
+/// Common functions like string spliting and comparing float numbers.
+
 #ifndef utils_h_
 #define utils_h_
 
 #include <string>
 #include <vector>
 
+/// This is to generalize LCA table and possibly change it in the future.
 using LCA_KEY = std::string;
 
+/// Common colors for colorful printing.
 const std::string RED = "\033[31m";
 const std::string BLUE = "\033[34m";
 const std::string GREEN = "\033[32m";
@@ -23,10 +29,24 @@ std::vector<std::string> split(const std::string &str);
 /// @return Vector of all substrings.
 std::vector<std::string> split(const std::string &str, char delimiter);
 
+/// Get the lca key for two leafs sort them and put in string "first"#"second".
+/// @param first First leaf.
+/// @param second Second leaf.
+/// @return LCA key for the hash table.
 LCA_KEY get_lca_key(int first, int second);
 
+/// Get the lca key for three leafs sort them and put in string
+/// "first"#"second"#"three.
+/// @param first First leaf.
+/// @param second Second leaf.
+/// @param third Third leaf.
+/// @return LCA key for the hash table.
 LCA_KEY get_lca_key(int first, int second, int third);
 
+/// Compare float value to "almost one".
+/// @param x Given value to comparing to one.
+/// @param tolerance How far from one it can be.
+/// @return Whether x is at most tolerance far away from 1.
 bool is_approx_one(float x, float tolerance = 1e-6f);
 
 #endif
