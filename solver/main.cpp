d70a3fe85df90984a3a897199db8339c71caf788
Tomas Turek <tom.turek@atlas.cz>
2026-05-15 16:11:04 +0200

    Removed LP computation.

--- solver/main.cpp
+++ solver/main.cpp
@@ -17,10 +17,10 @@ void process_input_file(const std::string &file) {
 
   // Contract cherries to reduce size.
   input.contract_cherries_chains();
-  auto lower_bound = lp(input);
-  std::cout << "# Lower bound: " << YELLOW << lower_bound << RESET << "."
-            << std::endl;
-  // Find the solution.
+  // auto lower_bound = lp(input);
+  // std::cout << "# Lower bound: " << YELLOW << lower_bound << RESET << "."
+  //           << std::endl;
+  //  Find the solution.
   auto edges_to_erase = ilp(input);
   // Remove the edges.
   auto output = input.remove_edges(edges_to_erase);
