e5d677aace83efc512ae391d0d408d35f064a531
Tomas Turek <tom.turek@atlas.cz>
2026-06-25 15:39:37 +0200

    Removed Highs solver.

--- solver/ilp.cpp
+++ solver/ilp.cpp
@@ -1,178 +1,9 @@
 #include "ilp.h"
-#include "lp_data/HighsStatus.h"
 #include "scip/scip_sol.h"
 #include "tree.h"
 #include "utils.h"
-#include <ranges>
 #include <set>
 #include <vector>
-
-////// ILP
-///
-/// ILP::ILP(Input &input) : input_(input), limit_(4) {
-///  highs_.setOptionValue("output_flag", false);
-///  components_ = std::vector<int>(input_.get_leaf_count() + 1, 1);
-///}
-///
-/// void ILP::initialize() {
-///  if (input_.get_reduced_leaf_count() <= 25) {
-///    limit_ = 25 * 25 * 25 * 25;
-///  }
-///  auto trios = std::vector<std::tuple<int, int, int>>();
-///  auto quartets = std::vector<std::tuple<int, int, int, int>>();
-///  input_.compute_trios_quartets(trios, quartets, limit_, components_);
-///
-///  auto number_of_edges = input_.get_node_count();
-///  auto number_of_rows = trios.size() + quartets.size() + 1;
-///  auto tree = input_.get_trees().at(0).get();
-///
-///  HighsModel model;
-///
-///  model.lp_.num_col_ = number_of_edges;
-///  model.lp_.col_cost_ = std::vector<double>(number_of_edges, 1.0);
-///  model.lp_.col_lower_ = std::vector<double>(number_of_edges, 0.0);
-///  model.lp_.col_upper_ = std::vector<double>(number_of_edges, 1.0);
-///
-///  model.lp_.num_row_ = number_of_rows;
-///  auto low = std::vector<double>(number_of_rows - 1, 1.0);
-///  low.push_back(0);
-///  model.lp_.row_lower_ = low;
-///  auto high = std::vector<double>(number_of_rows - 1, 1.0e30);
-///  high.push_back(input_.get_reduced_leaf_count() - 2);
-///  model.lp_.row_upper_ = high;
-///
-///  model.lp_.sense_ = ObjSense::kMinimize;
-///  model.lp_.a_matrix_.format_ = MatrixFormat::kRowwise;
-///
-///  std::vector<int> start = {0};
-///  std::vector<int> index;
-///
-///  for (auto &&trio : trios) {
-///    auto edges = tree->get_trio_edges(trio);
-///    index.insert(index.end(), edges.begin(), edges.end());
-///    start.push_back(index.size());
-///  }
-///
-///  for (auto &&quartet : quartets) {
-///    auto edges = tree->get_quartet_edges(quartet);
-///    index.insert(index.end(), edges.begin(), edges.end());
-///    start.push_back(index.size());
-///  }
-///
-///  for (int a = 0; a < number_of_edges; ++a) {
-///    index.push_back(a);
-///  }
-///  start.push_back(index.size());
-///
-///  std::vector<double> values(index.size(), 1.0);
-///  model.lp_.a_matrix_.start_ = start;
-///  model.lp_.a_matrix_.index_ = index;
-///  model.lp_.a_matrix_.value_ = values;
-///
-///  HighsStatus return_status = highs_.passModel(model);
-///  assert(return_status == HighsStatus::kOk);
-///
-///  const HighsLp &lp = highs_.getLp();
-///
-///  model.lp_.integrality_.resize(lp.num_col_);
-///  for (int col = 0; col < lp.num_col_; col++)
-///    model.lp_.integrality_[col] = HighsVarType::kInteger;
-///
-///  highs_.passModel(model);
-///  highs_.writeModel("my_model.lp");
-///}
-///
-/// std::set<int> ILP::run() {
-///  std::cout << "# Solving ILP with " << YELLOW << highs_.getNumRow() << RESET
-///            << " constraints and " << YELLOW << highs_.getNumCol() << RESET
-///            << " variables." << std::endl;
-///
-///  // Solve the model
-///  HighsStatus return_status = highs_.run();
-///  assert(return_status == HighsStatus::kOk);
-///
-///  // Get the model status
-///  const HighsModelStatus &model_status = highs_.getModelStatus();
-///  assert(model_status == HighsModelStatus::kOptimal);
-///
-///  const HighsInfo &info = highs_.getInfo();
-///  std::cout << "# Objective function value: " << YELLOW
-///            << info.objective_function_value << RESET << std::endl;
-///
-///  const HighsSolution &solution = highs_.getSolution();
-///  const HighsBasis &basis = highs_.getBasis();
-///
-///  const HighsLp &lp = highs_.getLp();
-///
-///  std::set<int> edges_to_erase;
-///  for (int col = 0; col < lp.num_col_; col++) {
-///    if (is_approx_one(solution.col_value[col])) {
-///      edges_to_erase.insert(col + 1);
-///    }
-///  }
-///  std::vector<int> indices;
-///  for (int a = 1; a < input_.get_node_count() + 1; ++a) {
-///    indices.push_back(a);
-///  }
-///  auto upper_bound = input_.get_reduced_leaf_count() - 2;
-///  std::vector<double> values = std::vector<double>(indices.size(), 1);
-///  HighsStatus status =
-///      highs_.addRow(edges_to_erase.size(), upper_bound, indices.size(),
-///                    indices.data(), values.data());
-///  return edges_to_erase;
-///}
-///
-/// void ILP::set_components(const std::vector<std::unique_ptr<Tree>> &output) {
-///  auto tree_count = 0;
-///  for (auto &&tree : output) {
-///    if (!tree->is_empty()) {
-///      ++tree_count;
-///      auto leafs = tree->get_leafs();
-///      for (auto &&taxa : leafs) {
-///        components_[taxa] = tree_count;
-///      }
-///    }
-///  }
-///}
-///
-/// bool ILP::update() {
-///  limit_ = input_.get_reduced_leaf_count() < 2 * limit_
-///               ? input_.get_reduced_leaf_count()
-///               : 2 * limit_;
-///
-///  auto trios = std::vector<std::tuple<int, int, int>>();
-///  auto quartets = std::vector<std::tuple<int, int, int, int>>();
-///  input_.compute_trios_quartets(trios, quartets, limit_, components_);
-///  if (trios.empty() && quartets.empty()) {
-///    return false;
-///  }
-///
-///  auto tree = input_.get_trees().at(0).get();
-///
-///  for (auto &&trio : trios) {
-///    auto edges = tree->get_trio_edges(trio);
-///    std::vector<int> indices;
-///    indices.insert(indices.end(), edges.begin(), edges.end());
-///    auto values = std::vector<double>(indices.size(), 1);
-///    HighsStatus status =
-///        highs_.addRow(1, 1.0e30, indices.size(), indices.data(),
-///        values.data());
-///    assert(status == HighsStatus::kOk);
-///  }
-///
-///  for (auto &&quartet : quartets) {
-///    auto edges = tree->get_quartet_edges(quartet);
-///    std::vector<int> indices;
-///    indices.insert(indices.end(), edges.begin(), edges.end());
-///    auto values = std::vector<double>(indices.size(), 1);
-///    HighsStatus status =
-///        highs_.addRow(1, 1.0e30, indices.size(), indices.data(),
-///        values.data());
-///    assert(status == HighsStatus::kOk);
-///  }
-///
-///  return true;
-///}
 
 /// SCIILP
 
@@ -213,7 +44,7 @@ SCIILP::~SCIILP() {
   }
 }
 
-void SCIILP::initialize(bool all_constraints) {
+void SCIILP::initialize() {
   // auto trios = std::vector<std::tuple<int, int, int>>();
   // auto quartets = std::vector<std::tuple<int, int, int, int>>();
   // input_.compute_trios_quartets(trios, quartets, limit_, components_,
@@ -245,7 +76,7 @@ void SCIILP::initialize(bool all_constraints) {
     SCIPaddCoefLinear(scip_, cons, vars_[b - 1], 1.0);
     SCIPaddCoefLinear(scip_, cons, vars_[c - 1], 1.0);
     SCIPaddCons(scip_, cons);
-    SCIPreleaseCons(scip_, &cons); // Release memory once added to SCIP
+    SCIPreleaseCons(scip_, &cons);
   }
 
   for (auto &&fork : extended_forks) {
@@ -260,29 +91,11 @@ void SCIILP::initialize(bool all_constraints) {
     SCIPaddCoefLinear(scip_, cons, vars_[fork[5] - 1], 1.0);
     SCIPaddCoefLinear(scip_, cons, vars_[fork[6] - 1], 1.0);
     SCIPaddCons(scip_, cons);
-    SCIPreleaseCons(scip_, &cons); // Release memory once added to SCIP
-    // SCIP_CONS *cons2;
-    // SCIPcreateConsBasicLinear(scip_, &cons2, "extended_fork_cons2", 0,
-    // nullptr,
-    //                           nullptr, 0.0, 3.0);
-    // SCIPaddCoefLinear(scip_, cons2, vars_[fork[1] - 1], 2.0);
-    // SCIPaddCoefLinear(scip_, cons2, vars_[fork[5] - 1], 1.0);
-    // SCIPaddCoefLinear(scip_, cons2, vars_[fork[6] - 1], 1.0);
-    // SCIPaddCons(scip_, cons2);
-    // SCIPreleaseCons(scip_, &cons2); // Release memory once added to SCIP
-    // SCIP_CONS *cons3;
-    // SCIPcreateConsBasicLinear(scip_, &cons3, "extended_fork_cons3", 0,
-    // nullptr,
-    //                           nullptr, 0.0, 3.0);
-    // SCIPaddCoefLinear(scip_, cons3, vars_[fork[2] - 1], 2.0);
-    // SCIPaddCoefLinear(scip_, cons3, vars_[fork[3] - 1], 1.0);
-    // SCIPaddCoefLinear(scip_, cons3, vars_[fork[4] - 1], 1.0);
-    // SCIPaddCons(scip_, cons3);
-    // SCIPreleaseCons(scip_, &cons3); // Release memory once added to SCIP
+    SCIPreleaseCons(scip_, &cons);
   }
 
   for (int i = 0; i < trios_.size(); ++i) {
-    if (i > input_.get_reduced_leaf_count() && trios_[i][3] > limit_) {
+    if (i > input_.get_reduced_leaf_count() / 2 || trios_[i][3] > limit_) {
       break;
     }
     auto trio = std::make_tuple(trios_[i][0], trios_[i][1], trios_[i][2]);
@@ -297,11 +110,11 @@ void SCIILP::initialize(bool all_constraints) {
       SCIPaddCoefLinear(scip_, cons, vars_[edge], 1.0);
     }
     SCIPaddCons(scip_, cons);
-    SCIPreleaseCons(scip_, &cons); // Release memory once added to SCIP
+    SCIPreleaseCons(scip_, &cons);
   }
 
   for (int i = 0; i < quartets_.size(); ++i) {
-    if (i > input_.get_reduced_leaf_count() && quartets_[i][3] > limit_) {
+    if (i > input_.get_reduced_leaf_count() / 2 || quartets_[i][3] > limit_) {
       break;
     }
     auto quartet = std::make_tuple(quartets_[i][0], quartets_[i][1],
@@ -363,8 +176,8 @@ std::set<int> SCIILP::run() {
   SCIP_RETCODE ret = SCIPsolve(scip_);
   assert(ret == SCIP_OKAY);
 
-  SCIP_STATUS status = SCIPgetStatus(scip_);
-  assert(status == SCIP_STATUS_OPTIMAL);
+  // SCIP_STATUS status = SCIPgetStatus(scip_);
+  // assert(status == SCIP_STATUS_OPTIMAL);
 
   SCIP_SOL *solution = SCIPgetBestSol(scip_);
 
@@ -393,6 +206,16 @@ std::set<int> SCIILP::run() {
   SCIPaddCons(scip_, new_cons);
   SCIPreleaseCons(scip_, &new_cons);
 
+  // Heuristics only!!
+  SCIP_CONS *cons_all_solved;
+  SCIPcreateConsBasicLinear(scip_, &cons_all_solved, "all_solved_edges", 0,
+                            nullptr, nullptr, obj_val, obj_val);
+  for (auto &&e : edges_to_erase) {
+    SCIPaddCoefLinear(scip_, cons_all_solved, vars_[e - 1], 1.0);
+  }
+  SCIPaddCons(scip_, cons_all_solved);
+  SCIPreleaseCons(scip_, &cons_all_solved);
+
   return edges_to_erase;
 }
 
@@ -406,7 +229,7 @@ bool SCIILP::update() {
   int counter = 0;
   // for (auto &&trio : trios) {
   for (int i = 0; i < trios_.size(); ++i) {
-    if (counter > input_.get_reduced_leaf_count()) {
+    if (counter > input_.get_reduced_leaf_count() / 2) {
       break;
     }
     if (components_[trios_[i][0]] != components_[trios_[i][1]] ||
@@ -435,7 +258,7 @@ bool SCIILP::update() {
 
   // for (auto &&quartet : quartets) {
   for (; i < quartets_.size(); ++i) {
-    if (counter > input_.get_reduced_leaf_count()) {
+    if (counter > input_.get_reduced_leaf_count() / 2) {
       break;
     }
     if (components_[quartets_[i][0]] != components_[quartets_[i][1]] ||
--- solver/ilp.h
+++ solver/ilp.h
@@ -5,7 +5,6 @@
 #ifndef ilp_h_
 #define ilp_h_
 
-#include "Highs.h"
 #include "tree.h"
 
 #include <scip/scip.h>
@@ -31,8 +30,7 @@ public:
   SCIILP(Input &input);
   ~SCIILP(); // Important: SCIP requires manual memory cleanup
   /// initialize the ilp with first constraints.
-  /// @param all_constraints If we want to add all_constraints.
-  void initialize(bool all_constraints);
+  void initialize();
   /// run the current ilp.
   /// @return set of edges that should be deleted.
   std::set<int> run();
@@ -44,34 +42,5 @@ public:
   /// @return true if ilp has new rows.
   bool update();
 };
-
-////// Class containing ILP model that is solving MAF.
-/// class ILP {
-/// public:
-///  /// Create empty ILP.
-///  /// @param input Which input to use.
-///  ILP(Input &input);
-///  /// initialize the ilp with first constraints.
-///  void initialize();
-///  /// run the current ilp.
-///  /// @return set of edges that should be deleted.
-///  std::set<int> run();
-///  /// update components based on the last result.
-///  /// @param output what is the forest created by the last solution.
-///  void set_components(const std::vector<std::unique_ptr<Tree>> &output);
-///  /// update ilp based on unsatisfied constraints.
-///  /// @return true if ilp has new rows.
-///  bool update();
-///
-/// private:
-///  /// Which model it is using.
-///  Highs highs_;
-///  /// What input we are solving.
-///  Input &input_;
-///  /// Setting limit for constraints.
-///  int limit_;
-///  /// Components of connected components.
-///  std::vector<int> components_;
-///};
 
 #endif
