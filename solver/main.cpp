76d093627c257c452b8a9eb0074d8f8c65ee9e8e
Tomas Turek <tom.turek@atlas.cz>
2026-06-25 16:29:14 +0200

    Removed redundant lines.

--- solver/main.cpp
+++ solver/main.cpp
@@ -1,42 +1,32 @@
 #include "ilp.h"
-#include "tree.h"
-#include "utils.h"
-#include <csignal>
-#include <iostream>
-#include <istream>
-#include <memory>
-
-void process(std::istream &is) {
-  auto input = Input(is);
-  input.compute_all_lca();
-  input.contract_cherries();
-
-  auto ilp = ILP(input);
-  ilp.initialize();
-
-  std::set<int> edges_to_erase;
-  std::vector<std::unique_ptr<Tree>> output;
-
-  do {
-    ilp.set_priorities();
-    edges_to_erase = ilp.run();
-    output = input.remove_edges(edges_to_erase);
-    ilp.set_components(output);
-  } while (ilp.update());
-
-  for (auto &&tree : output) {
-    if (!tree->is_empty()) {
-      tree->write(input.get_contractions());
-    }
-  }
-
-  std::cout << "# Size of the solution: " << VIOLET << edges_to_erase.size() + 1
-            << RESET << "." << std::endl;
-}
 
 int main(int argc, char **argv) {
   try {
-    process(std::cin);
+    auto input = Input(std::cin);
+    input.compute_all_lca();
+    input.contract_cherries();
+
+    auto ilp = ILP(input);
+    ilp.initialize();
+
+    std::set<int> edges_to_erase;
+    std::vector<std::unique_ptr<Tree>> output;
+
+    do {
+      ilp.set_priorities();
+      edges_to_erase = ilp.run();
+      output = input.remove_edges(edges_to_erase);
+      ilp.set_components(output);
+    } while (ilp.update());
+
+    for (auto &&tree : output) {
+      if (!tree->is_empty()) {
+        tree->write(input.get_contractions());
+      }
+    }
+
+    std::cout << "# Size of the solution: " << VIOLET
+              << edges_to_erase.size() + 1 << RESET << "." << std::endl;
     return 0;
   } catch (...) {
     std::cerr << "# Something went wrong." << std::endl;
