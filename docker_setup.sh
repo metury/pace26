fe31c3be9c47dc614f987ed6144d9558a366240b
Tomas Turek <tom.turek@atlas.cz>
2026-07-03 17:30:09 +0200

    Switch compilation to standard procedure without necessarily using musl.

--- docker_setup.sh
+++ docker_setup.sh
@@ -3,17 +3,7 @@
 set -ueo pipefail
 
 sudo apt-get update
-
-if sudo apt-get install -y scip libscip-dev; then
-  echo "========================================================"
-  echo "✅ SCIP installed successfully from APT repositories."
-  echo "========================================================"
-else
-  echo "========================================================"
-  echo "❌ SCIP was not found in your standard APT repositories."
-  echo "========================================================"
-  exit 1
-fi
+sudo apt-get install -y build-essential cmake scip libscip-dev
 
 mkdir build/
 cmake -B build/
